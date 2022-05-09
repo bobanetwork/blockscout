70fb340529edfd2312bdd8c0ee8bc819f6e86ea0
cby3149 <cby3149@gmail.com>
2022-05-09 15:04:27 -0700

    Revert "Change default token"
    This reverts commit 3672e294440b0945bc61efb93cc3dca3269ddc50.

--- apps/block_scout_web/lib/block_scout_web/views/address_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_view.ex
@@ -111,7 +111,7 @@ defmodule BlockScoutWeb.AddressView do
   def balance(%Address{fetched_coin_balance: nil}), do: ""
 
   def balance(%Address{fetched_coin_balance: balance}) do
-    format_wei_value(balance, :boba)
+    format_wei_value(balance, :ether)
   end
 
   def balance_percentage_enabled?(total_supply) do
